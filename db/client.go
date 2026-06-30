f99719d6dd19824b47a0ef6b97d88a5245e2e27b
zhengyaoqiu <yaoqiuzheng98@gmail.com>
2026-06-30 18:17:51 +0800

    add environment variable support for database connection and server port

--- db/client.go
+++ db/client.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,8 +13,12 @@ var _client *gorm.DB
 
 func init() {
 	var err error
+	host := getEnv("DB_HOST", "127.0.0.1")
+	port := getEnvInt("DB_PORT", 43306)
+	user := getEnv("DB_USER", "qiu")
+	pass := getEnv("DB_PASS", "123456")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=true&loc=Local",
-		"qiu", "123456", "127.0.0.1", 43306, Ride.String())
+		user, pass, host, port, Ride.String())
 	_client, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -21,4 +27,20 @@ func init() {
 
 func GetClient() *gorm.DB {
 	return _client
+}
+
+func getEnv(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return fallback
+}
+
+func getEnvInt(key string, fallback int) int {
+	if v := os.Getenv(key); v != "" {
+		if n, err := strconv.Atoi(v); err == nil {
+			return n
+		}
+	}
+	return fallback
 }
